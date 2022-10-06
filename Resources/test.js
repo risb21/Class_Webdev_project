2a7077317b8742380d2f03ac2b36da38d67ba909
Rishabh Chopde <rishikumarikuf@gmail.com>
2022-10-06 15:49:17 +0530

    Made popup function and put in forms

--- Resources/test.js
+++ Resources/test.js
@@ -1,6 +1,6 @@
 let login = document.getElementById("login");
 let overlay = document.getElementsByClassName("loginsignup")[0];
-let idk = document.getElementsByClassName("popup")[0];
+let popup = document.getElementsByClassName("popup")[0];
 
 console.log(typeof(overlay));
 console.log(overlay.style.display);
@@ -15,8 +15,38 @@ login.onclick = function () {
 };
 
 function hideOverlay() {
-    console.log(idk.onclick);
+    console.log(popup.onclick);
     overlay.style.display = "none";
 }
 
-overlay.onclick = function () {hideOverlay};
+let what = true;
+popup.onclick = function() { what = false; }
+
+overlay.onclick = function () {
+    if (what) {
+        overlay.style.display = "none";
+    } else {
+        what = true;
+    }
+};
+
+let toSign = document.getElementById("toSign");
+let toForm = document.getElementById("toLog");
+let signForm = document.getElementsByClassName("signupForm")[0];
+let logForm = document.getElementsByClassName("loginForm")[0];
+let formDiv = document.getElementsByClassName("form-div")[0];
+
+toSign.onclick = function () {
+    signForm.style.visibility = "visible";
+    signForm.style.height = "100%";
+    logForm.style.visibility = "hidden";
+    logForm.style.height = "0%";
+    formDiv.style.margin = " 0 0 0 60%";
+}
+toLog.onclick = function () {
+    signForm.style.visibility = "hidden";
+    signForm.style.height = "0%";
+    logForm.style.visibility = "visible";
+    logForm.style.height = "100%";
+    formDiv.style.margin = "0 0 0 0%";
+}
