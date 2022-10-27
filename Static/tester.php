5a84e1e1a7c4914ea13d48beef867018fc36eb96
Rishabh Chopde <rishikumarikuf@gmail.com>
2022-10-27 14:53:20 +0530

    Added Uniqueness of board checking function
    Each finished puzzle needs to be checked for uniqueness, or else even
    though the user submits the right board, it is considered wrong. Here I
    just ran the puzzle maker till the board was unique and every 10 tries I
    changed the filled in board itself. The minimum number of values still
    in the board has to be 28 or else the script will time out. Will need to
    implement a queue either in program memory or maintained on the database
    I have to manage rn, will take 3 and a half hours (roughly) to compute
    so it is probably gonna be stored in the DB. I have to eat lunch.

--- Static/tester.php
+++ Static/tester.php
@@ -7,7 +7,11 @@
                 if (!$j) {
                     echo "|";
                 }
-                echo '&nbsp&nbsp&nbsp'.$board[$i][$j];
+                if ($board[$i][$j]) {
+                    echo '&nbsp&nbsp&nbsp'.$board[$i][$j];
+                } else {
+                    echo '&nbsp&nbsp&nbsp&nbsp&nbsp';
+                }
                 if (!($j % 3 - 2)) {
                     echo "|";
                 }
@@ -45,64 +49,149 @@
         return $box;
     }
 
-    // Recursive Backtracking function to fill the board
-    
-    function Solver(&$board, $rowInd, $colInd) {
-        if ($rowInd > 8) {
-            return true;    // Checks if board is full
-        }
-
-        $validNums = [1,2,3,4,5,6,7,8,9];
+    function validValues($board, $rowInd, $colInd) {
+        $validVals = [1,2,3,4,5,6,7,8,9];
         $row = getRow($board, $rowInd);
         $col = getCol($board, $colInd);     // Gets values in row, column and box to follow sudoku rules
         $box = getBox($board, $rowInd, $colInd);
 
         foreach ($row as $val) {
             if ($val) {
-                $validNums = array_diff($validNums, [$val]);
+                $validVals = array_diff($validVals, [$val]);
             }   // array_diff will remove specified value from the array like a set operation
         }
         foreach ($col as $val) {
             if ($val) {
-                $validNums = array_diff($validNums, [$val]);
+                $validVals = array_diff($validVals, [$val]);
             }
         }
         foreach ($box as $val) {
             if ($val) {
-                $validNums = array_diff($validNums, [$val]);
+                $validVals = array_diff($validVals, [$val]);
             }
         }
+        return $validVals;
+    }
 
-        while (!empty($validNums)) {
-
-            $board[$rowInd][$colInd] = $validNums[array_rand($validNums, 1)];
-            /* array_rand will return random index and not value from parsed array, 
-               1 signifies no. of indices to choose */
-            
-            /* Weird indices just to increment without breaking bounds
-               (except row index in very last call, handled in first conditional in this function) */
+    // Recursive Backtracking function to fill the board
+    
+    function Solver(&$board, $rowInd, $colInd) {
+        if ($rowInd > 8) {
+            return true;    // Checks if board is full
+        }
+        if ($board[$rowInd][$colInd] == 0){
+            $validNums = validValues($board, $rowInd, $colInd);
+    
+            while (!empty($validNums)) {
+    
+                $board[$rowInd][$colInd] = $validNums[array_rand($validNums, 1)];
+                /* array_rand will return random index and not value from parsed array, 
+                   1 signifies no. of indices to choose */
+                
+                /* Weird indices just to increment without breaking bounds
+                   (except row index in very last call, handled in first conditional in this function) */
+                if (Solver($board, $rowInd + floor(($colInd + 1)/9), ($colInd + 1) % 9)) {
+                    return true;
+                }
+                
+                // Will only end up here if value randomly set was not valid down the line or even immediately
+                $validNums = array_diff($validNums, [$board[$rowInd][$colInd]]);
+                $board[$rowInd][$colInd] = 0;
+            }
+            return false;
+        } else {
             if (Solver($board, $rowInd + floor(($colInd + 1)/9), ($colInd + 1) % 9)) {
                 return true;
             }
-            
-            // Will only end up here if value randomly set was not valid down the line or even immediately
-            $validNums = array_diff($validNums, [$board[$rowInd][$colInd]]);
-            $board[$rowInd][$colInd] = 0;
         }
-        return false;
     }
 
+    function isValid($board, $puzzle) {
+        if (Solver($puzzle, 0, 0) == $board) { return true; }
+        return false;
+    }
+    
+    function puzzleMaker($board, &$puzzle, $difficulty) {
+
+        // Mapping difficulty from 0 to 1 to number of iterations
+        $diffMap = floor($difficulty * 53); 
+        while ($diffMap) {
+            
+            // Random indices to remove
+            $randRow = rand() % 9;
+            $randCol = rand() % 9;
+    
+            if ($puzzle[$randRow][$randCol]) {
+
+                $oldState = $puzzle[$randRow][$randCol];
+                $puzzle[$randRow][$randCol] = 0;
+                if (isValid($board, $puzzle)) { 
+                    --$diffMap;
+                } else {
+                    $puzzle[$randRow][$randCol] = $oldState;
+                }
+            }
+        }
+    }
+
+    function isUnique($board) {
+        $SolvedOnce = $board;
+        Solver($SolvedOnce, 0, 0);
+        
+        $SolvedAgain = $board;
+        for ($i = 0; $i < 3; $i++) {
+            Solver($SolvedAgain, 0, 0);
+            if ($SolvedOnce != $SolvedAgain) {
+                return false;
+            }
+            $SolvedAgain = $board;
+        }
+        return true;
+    }
+
+    $difficulty = 1;
+    
     $start = microtime(true);   // Calculating time at the start of computation
     $board = array(array());
-
+    
     for ($i = 0; $i < 9; $i++) {
         for ($j = 0; $j < 9; $j++) {
             $board[$i][$j] = 0;
         }
     }
-
+    
     Solver($board, 0, 0);
     printer($board);
-    echo 'Done in: ' . (microtime(true) - $start) * 1000 . 'ms';
+    $end = microtime(true);
+    echo 'Done in: ' . ($end - $start) * 1000 . 'ms';
+    
+    $puzzle = $board;
+    $start = microtime(true);   // Calculating time at the start of computation
+    puzzleMaker($board, $puzzle, $difficulty);
+    
+    $count = 0;
+    while (!isUnique($puzzle)) {
+        if ($count % 10) {
+            $puzzle = $board;
+            puzzleMaker($board, $puzzle, $difficulty);
+        } else {
+            for ($i = 0; $i < 9; $i++) {
+                for ($j = 0; $j < 9; $j++) {
+                    $board[$i][$j] = 0;
+                }
+            }
+            Solver($board, 0, 0);
+            $puzzle = $board;
+            puzzleMaker($board, $puzzle, $difficulty);
+        }
+        $count++;
+    }
 
+    $end = microtime(true);
+    echo "<br>";
+    printer($board);
+    printer($puzzle);
+    echo 'Took ' . $count . ' iterations<br>';
+    echo 'Done in: ' . $end - $start . ' s<br>';
+    
 ?>
